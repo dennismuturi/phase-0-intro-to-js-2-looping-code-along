cc73167389972abecb70f63c577c5f9f4d4e22d3
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-03 17:06:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names,eventType)
+{
+    const messages=[];
+    for (let i = 0; i < names.length; i++)
+    {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${eventType} gift!`
+    }
+    return messages;
+}
+
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
